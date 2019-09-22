0111ebd13b5bee33fe2d17f2fbe3b2077172b44a
withJ <ehdgodgka@gmail.com>
2019-09-22 22:58:25 +0900

    create: 과제2 - 2.1 Find Odd Integer

---
+++ w3_JS2/assignment02_algorithm_problems/find_odd_integer.js
@@ -0,0 +1,19 @@
+function findOddInt (n) { 
+    var count = 1;
+    var numNow= n[0];// 세고있는 수 초기화 
+// 현재 세고있는수
+    for(let i=1; i<n.length;i++){
+        if(numNow===n[i]){
+            count++;
+        }else{
+            if(count%2===1){
+                break;
+            }
+            numNow=n[i];
+            count=1; 
+        }
+    }
+// 현재 센 갯수 
+
+    return numNow ;
+}
