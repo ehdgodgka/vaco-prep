73fbe1d6fda5fe94512c4fd9b822639fc0d265f2
withJ <ehdgodgka@gmail.com>
2019-10-09 22:05:07 +0900

    w8_todolist -add: todo 삭제 기능

--- w7_Advance Js2/assignment05_calendar_project/calendar.js
+++ w7_Advance Js2/assignment05_calendar_project/calendar.js
@@ -170,12 +170,13 @@ function todolistInit() {
   while (todolist.firstChild) {
     todolist.removeChild(todolist.firstChild);
   }
-
+  thisTodolist=[];
   // DO 2:현재 날짜에 TODO 리스트가 있으면 보여준다. 수도 출력 .
   showThisdateTodolist();
   // DO 1:todolist의 기능: todo추가,현재 상태 저장
   //TODO 4: 완료 미완료 필터링 ,날짜별 todo 클리어 기능을 등록합니다.
   setTodolistFunction();
+
   // TODO 3:todo 체크 ,수정,삭제 , 현재 상태 저장
   setTodoFunction();
 }
@@ -270,9 +271,12 @@ function addTodo(string, check) {
   var todolist = document.querySelector('.todolist-list');
   var checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-
   var todo = document.createElement('div');
   var todoText = document.createTextNode(string);
+  var deleteButton=document.createElement('span');
+  deleteButton.textContent='✖';
+  deleteButton.classList.add('button-delete');
+  deleteButton.style.display='none';
   todo.classList.add('todo');
   if (check) {
     todo.classList.add('done');
@@ -280,6 +284,7 @@ function addTodo(string, check) {
   }
   todo.appendChild(checkbox);
   todo.appendChild(todoText);
+  todo.appendChild(deleteButton);
   todolist.appendChild(todo);
   printTodoLeft();
 
@@ -291,30 +296,87 @@ function saveThisList() {
   todolistbyDate[setTodolistDate()] = JSON.stringify(thisTodolist);
   console.log(todolistbyDate);
 }
+
+
+
+
 function setTodoFunction() {
   //DO 3-1: 체크박스에 상태에 따라 check클래스를 토글을 시키도록 하자.
   // check 클래스의 스타일에느 가운데 줄이 그이고 현재리스트의 체크여부, 전체리스트를 업데이트 합니다.
   checkTodo();
-  //TODO 3-2: 할일에 커서 올리면 생성되는 x 버튼으로 삭제할수있게 합니다. 현재리스트와 전체리스트를 업데이트 합니다
+  //DO 3-2: 할일에 커서 올리면 생성되는 x 버튼으로 삭제할수있게 합니다. 현재리스트와 전체리스트를 업데이트 합니다
+  deleteTodo();
   //TODO 3-3: 할일을 더블 클릭으로 수정할수있게 합니다 현재리스트와 전체리스트를 업데이트 합니다.
+  modifyTodo();
 }
 
 function checkTodo() {
   var todos = document.querySelector('.todolist-list');
   todos.addEventListener('click', (event) => {
-    var index = Array.from(todos.children).indexOf(event.target.parentElement);
-    if (event.target.checked) {
-      event.target.parentElement.classList.add('done');
-      thisTodolist[index].done = true;
-    } else {
-      event.target.parentElement.classList.remove('done');
-      thisTodolist[index].done = false;
+    if(event.target.tagName==='INPUT'){
+        var index = Array.from(todos.children).indexOf(event.target.parentElement);
+        if (event.target.checked) {
+          event.target.parentElement.classList.add('done');
+          thisTodolist[index].done = true;
+        } else {
+          event.target.parentElement.classList.remove('done');
+          thisTodolist[index].done = false;
+        }
+        printTodoLeft();
+        saveThisList();
     }
-    printTodoLeft();
-    saveThisList();
+ 
   });
 }
-function filterTodolist() {}
+function deleteTodo(){
+    //hover 시에 삭제 버튼이 나타나게하기
+         //DO 3-2:할일을 삭제합니다
+    var todos = document.querySelector('.todolist-list');
+    todos.addEventListener('click',event=>
+    {
+        if(event.target.classList.contains('button-delete')){
+            console.log('delete!!');
+            var index=Array.from(todos.children).indexOf(event.target.parentElement);
+            todos.removeChild(event.target.parentElement);
+            // 없앤애의 인텍스를 받아서 배열에서 slice 해야한다
+            thisTodolist.splice(index,1);
+            saveThisList();
+        }
+    }
+    );
+    todos.addEventListener("mouseover",event=>
+    {   var deleteButton;
+        if(event.target.classList.contains('todo')||event.target.parentElement.classList.contains('todo')){
+            if(event.target.classList.contains('todo')){
+                deleteButton = event.target.querySelector('.button-delete');
+            }else{
+                deleteButton = event.target.parentElement.querySelector('.button-delete');
+            }
+        }
+            console.log('mouseover');
+            deleteButton.style.display = 'inline';
+    }
+    );
+
+    todos.addEventListener('mouseout',event=>
+    {
+        if(event.target.classList.contains('todo')||event.target.parentElement.classList.contains('todo')){
+            console.log('mouseout');
+            if(event.target.classList.contains('todo')){
+                deleteButton = event.target.querySelector('.button-delete');
+            }else{
+                deleteButton = event.target.parentElement.querySelector('.button-delete');
+            }
+            deleteButton.style.display='none';
+        }
+    });
+
+
+}
+
+function modifyTodo(){
+    
+}
 
 calendarInit();
 todolistInit();
