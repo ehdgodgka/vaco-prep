9884c65f9fb103b4d93e880f78c4e52e1071fc17
withJ <ehdgodgka@gmail.com>
2019-12-18 22:30:16 +0900

    해커뉴스 비동기 문제 해결함! - data를 get 함수에서 저장 - 저장한 데이터를 dom 에 보여줌

--- HackerNews/hackerNews2.js
+++ HackerNews/hackerNews2.js
@@ -1,7 +1,7 @@
 var tableBody = document.querySelector('.articles');
 var page = 1;
 var stories;
-
+var datasave = [];
 // 테이블 생성
 for (var row = 0; row < 30; row++) {
   var article = document.createElement('tr');
@@ -25,10 +25,8 @@ for (var row = 0; row < 30; row++) {
   tableBody.appendChild(article);
   tableBody.appendChild(articleDetail);
 }
-
 moreBt();
 getData();
-
 // 다음 페이지로
 function moreBt() {
   //moreBt 이 눌렸을 때  다음 페이지 컨텐츠
@@ -36,43 +34,55 @@ function moreBt() {
   var moreBt = document.querySelector('.more');
   moreBt.addEventListener('click', nextData);
 }
-function nextData(){
-    page++;
-    getData();
+function nextData() {
+  datasave = [];
+  page++;
+  getData();
 }
-
 function getData() {
-
+  //약 500개의 top 스토리 id 반환
   $.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty', function(data, textStatus, jqXHR) {
-    //약 500개의 top 스토리 id 반환
     // console.log(data, textStatus, jqXHR);
 
     // 30개의 스토리
     stories = data.slice(30 * (page - 1), 30 * (page - 1) + 30);
+    for (var index = 0; index < 30; index++) {
+      $.get(
+        'https://hacker-news.firebaseio.com/v0/item/' + stories[index] + '.json?print=pretty',
+        (function(data) {
+          var i = index;
+          return function inner(data) {
+            datasave.push(data);
+            if (i === 29) {
+              draw();
+            }
+          };
+        })()
+      );
+    }
 
-    //스토리 데이터
-    for (var index = 0; index <30; index++) {
-        callFunc(index);
-       function callFunc (index){
-        $.get('https://hacker-news.firebaseio.com/v0/item/' + stories[index] + '.json?print=pretty', function(data) {
-        
+    function draw() {
+      for (var index = 0; index < 30; index++) {
+        console.log(datasave[index]);
+        var data = datasave[index];
+
         var tableRow = document.getElementById(index + 1);
         var detailRow = tableRow.nextSibling;
-          var detail = detailRow.querySelector('.detail');
+        var detail = detailRow.querySelector('.detail');
         var title = tableRow.querySelector('.title');
-          //story title,link
-          var url = data.url ? data.url.split('/')[2] : '';
-          if (url.includes('www.')) {
-            url = url.replace('www.', '');
-          }
-          var link = document.createElement('a');
-          link.textContent = url ? '(' + url + ')' : '';
-          link.href = data.url;
-          setTimeout(function(){
-          title.innerHTML = '<a href=' + data.url + " class='link'>" + '▲' + data.title + '</a>';
-          title.appendChild(link);
-          //story detail
-          detail.innerHTML =
+        //story title,link
+        var url = data.url ? data.url.split('/')[2] : '';
+        if (url.includes('www.')) {
+          url = url.replace('www.', '');
+        }
+        var link = document.createElement('a');
+        link.textContent = url ? '(' + url + ')' : '';
+        link.href = data.url;
+
+        title.innerHTML = '<a href=' + data.url + " class='link'>" + '▲' + data.title + '</a>';
+        title.appendChild(link);
+        //story detail
+        detail.innerHTML =
           ' ' +
           data.score +
           ' points by ' +
@@ -81,29 +91,24 @@ function getData() {
           timeCal(data.time) +
           ' | hide | ' +
           (data.descendants === 0 ? 'discuss' : data.descendants === 1 ? '1 comment' : data.descendants + ' comments');
-           },0);
-        });
-      
+      }
+      //인덱스 수정
+      var index = document.querySelectorAll('.index');
+      index.forEach((item, index) => {
+        item.textContent = (page - 1) * 30 + index + 1 + '.';
+      });
+    }
 
+    function timeCal(Timestamp) {
+      //story 올린후 지난 시간 계산 //  unix 시간 변환
+      var TimestampNow = Math.round(new Date().getTime() / 1000);
+      var seconds = TimestampNow - Timestamp;
+
+      return seconds / 3600 >= 1
+        ? Math.floor(seconds / 3600) === 1
+          ? '1 hour ago'
+          : Math.floor(seconds / 3600) + ' hours ago'
+        : Math.floor(seconds * 60) + ' mins ago';
     }
-      
-    }
-    //인덱스 수정
-    var index = document.querySelectorAll('.index');
-    index.forEach((item, index) => {
-      item.textContent = (page - 1) * 30 + index + 1 + '.';
-    });
   });
-}
-
-function timeCal(Timestamp) {
-  //story 올린후 지난 시간 계산 //  unix 시간 변환
-  var TimestampNow = Math.round(new Date().getTime() / 1000);
-  var seconds = TimestampNow - Timestamp;
-
-  return seconds / 3600 >= 1
-    ? Math.floor(seconds / 3600) === 1
-      ? '1 hour ago'
-      : Math.floor(seconds / 3600) + ' hours ago'
-    : Math.floor(seconds * 60) + ' mins ago';
 }
